23812a1a86154bcb331659c5d8b37a88858f6d7b
swordlet <tricorelzj@gmail.com>
2022-11-05 21:22:42 +0800

    add wallet address in log output, add log file option in config.

--- server/logger/logger.go
+++ server/logger/logger.go
@@ -104,3 +104,11 @@ func (l *Logger) Debugln(v ...interface{}) {
 
 	l.Logger.Println(v...)
 }
+
+func CheckFileExist(filename string) bool {
+	exist := true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		exist = false
+	}
+	return exist
+}
